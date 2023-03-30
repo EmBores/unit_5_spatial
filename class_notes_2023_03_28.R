9f4ab2ed395841337fdf3a50afe8f3188f2afc50
EmBores <ebores@email.sc.edu>
2023-03-30 11:24:20 -0400

    03/28 and 03/30 notes

--- class_notes_2023_03_28.R
+++ class_notes_2023_03_28.R
@@ -1,5 +1,6 @@
 ##EMB
 ##2023-03-28
+##2023-03-30
 
 
 library(tidyverse)
@@ -57,8 +58,11 @@ GOM_chl_map= ggplot()+
 
 #bathymetry
 #Gulf of Main
+#class 2023-3-30
 lon_bound=c(-72, -62)
 lat_bound=c(39,47)
+
+world_map=map_data("worldHires")
 
 bath_m_raw= marmap::getNOAA.bathy(lon1=lon_bound[1],
                       lon2=lon_bound[2],
@@ -67,3 +71,100 @@ bath_m_raw= marmap::getNOAA.bathy(lon1=lon_bound[1],
                       resolution=4) #in arcminutes
 class(bath_m_raw)
 
+#convert to df
+bath_m_df=marmap::fortify.bathy(bath_m_raw)
+head(bath_m_df)
+bath_m= bath_m_df %>%
+  mutate(depth_m= ifelse(z>20, NA, z)) %>%
+  dplyr::select(-z)
+head(bath_m)
+tail(bath_m)
+
+ggplot()+
+  geom_raster(data=bath_m, aes(x=x, y=y, fill=depth_m))+
+  geom_polygon(data=world_map, aes(x=long, y=lat, group=group), fill="black")+
+  coord_fixed(1.3, xlim=lon_bound, ylim=lat_bound, expand=FALSE)+
+  scale_fill_gradientn(colors=c("black", "darkblue", "lightblue"),
+                       values= scales::rescale(c(-6000, -300, 0)),
+                       name="Depth(m)") #values being anchored to the colors above
+
+#contour lines
+ggplot()+
+  geom_raster(data=bath_m, aes(x=x, y=y, fill=depth_m))+
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), breaks=c(-100),
+               linewidth=c(0.25), color="grey")+
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), breaks=c(-200),
+               linewidth=c(0.5), color="grey")+
+  geom_contour(data=bath_m, aes(x=x, y=y, z=depth_m), breaks=c(-500),
+               linewidth=c(0.75), color="grey")+ 
+  geom_polygon(data=world_map, aes(x=long, y=lat, group=group), fill="black")+
+  coord_fixed(1.3, xlim=lon_bound, ylim=lat_bound, expand=FALSE)+
+  scale_fill_gradientn(colors=c("black", "darkblue", "lightblue"),
+                       values= scales::rescale(c(-6000, -300, 0)),
+                       name="Depth(m)")+
+  theme_bw()
+  #breaks contour line around depth 100
+
+#rasterize the bathy object
+bath_m_raster= marmap::as.raster(bath_m_raw)
+
+names(bath_m_raster)="bath_m"
+
+#resample
+
+bath_layer_chl_dims=raster::resample(bath_m_raster, chl_GOM_raster) 
+  
+raster_stack=stack(chl_GOM_raster, bath_layer_chl_dims)  
+
+plot(raster_stack) 
+
+stack_df= data.frame(raster::rasterToPoints(raster_stack))   
+head(stack_df)  
+
+#O'Reilly paper
+oligo_chl_a= 0.1
+eutro_chl_a= 1.67
+
+stack_df=stack_df %>%
+  mutate(trophic_index=case_when(chl_a< oligo_chl_a ~ "oligotrophic",
+         chl_a>oligo_chl_a & chl_a < eutro_chl_a ~ "mesotrophic",
+         chl_a >eutro_chl_a ~ "eutrophic"))%>%
+  mutate(trophic_index=as.factor(trophic_index))
+head(stack_df)
+tail(stack_df)
+summary(stack_df)
+
+ggplot()+
+  geom_raster(data=stack_df, aes(x=x, y=y, fill=trophic_index))+
+  geom_polygon(data=world_map, aes(x=long, y=lat, group=group), fill="black")+
+  coord_fixed(1.3, xlim=lon_bound, ylim=lat_bound, expand=FALSE)+
+  theme_bw()
+
+stack_df %>%
+  group_by(trophic_index) %>%
+  summarize (mean_bath_m= mean(bath_m))
+
+library(sf)
+
+USA_crit_hab=st_read(dsn="data/North_Atlantic_Right_Whale_Critical_Habitat/",
+                     layer="North_Atlantic_Right_Whale_Critical_Habitat")
+ 
+
+USA_crit_hab_sf=st_transform(USA_crit_hab, crs=4326)
+
+CAN_crit_hab= read.csv("data/NARW_canadian_critical_habitat_2017.csv")
+head(CAN_crit_hab)
+
+CAN_crit_hab_sf= CAN_crit_hab %>%
+  st_as_sf(coords=c("lon", "lat"), crs=4326) %>%
+  dplyr::group_by(habitat, country)%>%
+  dplyr::summarize(do_union=FALSE) %>%
+  st_cast("POLYGON")
+
+USA_crit_hab_sf$habitat= c("GOM", "SEUS")
+USA_crit_hab_sf$country= "USA"
+
+USA_crit_hab_sf= USA_crit_hab_sf %>%
+  dplyr::select(country, habitat, geometry)
+
+crit_hab= rbind(USA_crit_hab_sf, CAN_crit_hab_sf)
